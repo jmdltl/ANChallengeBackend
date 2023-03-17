0d1aed481effad2ba93ce1767f75d8198ac148b8
Juan Miguel de la Torre Loza <migueldlt@bluetrailsoft.com>
2023-03-16 23:29:21 -0600

    Added Cors

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.enableVersioning({
     type: VersioningType.URI,
   });
